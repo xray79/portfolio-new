54c77d78cb2626f9b623c7ac0ec05fe29746d3aa
Ibnul Huq <ibnul.huq@gmail.com>
2022-08-02 10:41:10 +0100

    add responsive navbar

--- components/Logos.js
+++ components/Logos.js
@@ -2,8 +2,6 @@ import Image from "next/image";
 import styles from "../styles/Logos.module.css";
 
 const Logos = (props) => {
-  console.log(props.imgUrl);
-
   return (
     <div className={styles.logos}>
       <button>
