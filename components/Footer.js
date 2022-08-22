30e8d3fad17d7f34ec54e9b1bcb6025965d2797a
Ibnul Huq <ibnul.huq@gmail.com>
2022-08-22 10:05:19 +0100

    add responsive navbar

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,13 @@
 import styles from "../styles/Footer.module.scss";
 
 export const Footer = () => {
+  const date = new Date().getFullYear();
+
   return (
-    <section id="footer">
-      <div className={styles.container}>Footer</div>
+    <section className={styles.footer} id="footer">
+      <div className={styles.container}>
+        <h2 className={styles.heading}>Copyright {date} Ibnul Huq</h2>
+      </div>
     </section>
   );
 };
